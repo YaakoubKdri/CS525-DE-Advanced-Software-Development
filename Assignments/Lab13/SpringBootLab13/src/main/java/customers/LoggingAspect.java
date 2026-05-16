f5d787fc2557f03e7ab5717304fd7807cb9dc2ed
kadri <kadriyaakoub@gmail.com>
2026-05-16 00:58:56 -0500

    Lab 13 --> Part a: AOP --> question d

--- Assignments/Lab13/SpringBootLab13/src/main/java/customers/LoggingAspect.java
+++ Assignments/Lab13/SpringBootLab13/src/main/java/customers/LoggingAspect.java
@@ -1,9 +1,12 @@
 package customers;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.time.LocalDate;
 
@@ -20,5 +23,16 @@ public class LoggingAspect {
                 + " message=" + message
                 + " outgoing mail server=" + emailSender.getOutgoingMailServer()
         );
+    }
+
+    @Around("execution(* customers.CustomerDAO.*(..))")
+    public Object measureMethodTime(ProceedingJoinPoint call) throws Throwable{
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start(call.getSignature().getName());
+        Object returnValue = call.proceed();
+        stopWatch.stop();
+        long totalTime = stopWatch.getLastTaskTimeMillis();
+        System.out.println("Execution time of " + call.getSignature().getName() + " = " + totalTime + " ms");
+        return returnValue;
     }
 }
